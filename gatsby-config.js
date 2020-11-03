b52f5a556069864be49bf207f9f458c0c507eb64
Ramon Carceles <juanramoncarceles@gmail.com>
2020-11-03 20:39:39 +0100

    feat: configured several plugins and added siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,67 @@
 module.exports = {
   siteMetadata: {
-    title: `portfolio`,
-    description: `Valeria Lovato's design portfolio. Checkout my architecture and UI UX projects.`,
-    author: `@valerialovato`,
+    title: "portfolio",
+    description: "Valeria Lovato's design portfolio. Checkout my architecture and UI UX projects.",
+    author: "@valerialovato",
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-react-helmet",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `images`,
+        name: "images",
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/images/gatsby-icon.png", // Path relative to the root of the site.
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `src`,
-        path: `${__dirname}/src/`,
+        name: "pages",
+        path: `${__dirname}/src/pages`,
       },
     },
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-emotion`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "post",
+        path: `${__dirname}/posts`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "project",
+        path: `${__dirname}/projects`,
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 800,
+            },
+          },
+        ],
+      },
+    },
+    "gatsby-plugin-emotion",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
