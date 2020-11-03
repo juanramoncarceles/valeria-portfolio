1059d10bb6637bcdba803f4eeaa4694c3ffddcd7
Ramon Carceles <juanramoncarceles@gmail.com>
2020-11-03 20:41:27 +0100

    feat: logic to filter the markdown files by sourceInstanceName and create pages for posts and projects individually.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,49 +1,89 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`);
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
+  const { createNodeField } = actions;
+  if (node.internal.type === "MarkdownRemark") { // If using MDX change for "Mdx".
+    const parent = getNode(node.parent);
+    const collection = parent.sourceInstanceName;
+    // The slug is created as the name of the file or parent folder in case of files called index.
+    const slug = createFilePath({ node, getNode });
+    // Add field 'collection' (type) to the node.
     createNodeField({
       node,
-      name: `slug`,
+      name: "collection",
+      value: collection,
+    });
+    // Add field 'slug' to the node.
+    createNodeField({
+      node,
+      name: "slug",
       value: slug,
-    })
+    });
   }
 }
 
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  const result = await graphql(`
+  const { createPage } = actions;
+  
+  // THE POSTS
+  const {
+    data: {
+      allMarkdownRemark: {
+        nodes: posts
+      }
+    }
+  } = await graphql(`
     query {
-      allMarkdownRemark {
-        edges {
-          node {
-            fields {
-              slug
-            }
+      allMarkdownRemark(filter: {fields: {collection: {eq: "post"}}}) {
+        nodes {
+          id
+          fields {
+            slug
           }
         }
       }
     }
-  `)
+  `);
 
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  posts.forEach( post => {
     createPage({
-      path: node.fields.slug,
-      component: path.resolve(`./src/templates/blog-post.js`),
+      path: `blog${post.fields.slug}`, // Another option is to use the frontmatter title to create the slug parsing it.
+      component: path.resolve("./src/templates/post.js"),
       context: {
-        // Data passed to context is available
-        // in page queries as GraphQL variables.
-        slug: node.fields.slug,
+        id: post.id,
       },
-    })
-  })
+    });
+  });
+
+
+  // THE PROJECTS
+  const {
+    data: {
+      allMarkdownRemark: {
+        nodes: projects
+      }
+    }
+  } = await graphql(`
+    query {
+      allMarkdownRemark(filter: {fields: {collection: {eq: "project"}}}) {
+        nodes {
+          id
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  `);
+  
+  projects.forEach( project => {
+    createPage({
+      path: `project${project.fields.slug}`,
+      component: path.resolve("./src/templates/project.js"),
+      context: {
+        id: project.id,
+      },
+    });
+  });
 }
