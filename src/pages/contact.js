0dd8bf63e9a4f916bcf3d34be4cba178710786ab
Ramon Carceles <juanramoncarceles@gmail.com>
2020-11-03 21:10:57 +0100

    feat: created contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const Contact = () => {
+  return (
+    <Layout>
+      <SEO title="Contact" />
+      <div>Thid is the contact page</div>
+    </Layout>
+  );
+};
+
+export default Contact;
