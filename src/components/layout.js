9673c1ff56e6bbdffb7c3f0c7b2928359c5d9632
Ramon Carceles <juanramoncarceles@gmail.com>
2020-11-07 16:08:06 +0100

    Removed inline styles still from the boilerplate.

--- src/components/layout.js
+++ src/components/layout.js
@@ -42,9 +42,6 @@ const Layout = ({ children, pageTitle, fullHeightHeader }) => {
           />
           <main
             style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `0 1.0875rem 1.45rem`,
               flexGrow: 1,
             }}
           >
