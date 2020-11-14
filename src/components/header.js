e0e9316ea2c947218ed35bee0769c2e89043c386
Ramon Carceles <juanramoncarceles@gmail.com>
2020-11-14 13:53:13 +0100

    Improvement in the logic and styles for the two options of the Header position.

--- src/components/header.js
+++ src/components/header.js
@@ -5,35 +5,50 @@ import PropTypes from "prop-types";
 import ThemeContext from "../context/ThemeContext";
 import headerStyles from "./header.module.css";
 
-const Header = ({ siteTitle }) => (
-  <ThemeContext.Consumer>
-    {theme => (
-      <header className={headerStyles.root}>
-        <div className={headerStyles.navContainer}>
-          <Link to="/" className={headerStyles.siteTitle}>
-            {siteTitle.toLowerCase()}
-          </Link>
-          <nav className={headerStyles.nav}>
-            <Link to="/">portfolio</Link>
-            <Link to="/about/">about me</Link>
-            <Link to="/contact/">contact</Link>
-            <Link to="/blog/">blog</Link>
-          </nav>
-        </div>
-        {false ? (
-          <button className="dark-switcher" onClick={theme.toggleDark}>
-            {theme.dark ? <span>Light mode ☀</span> : <span>Dark mode ☾</span>}
-          </button>
-        ) : (
-          ""
-        )}
-      </header>
-    )}
-  </ThemeContext.Consumer>
-);
+const Header = ({ siteTitle, absolute }) => {
+  return (
+    <ThemeContext.Consumer>
+      {theme => (
+        <header
+          className={`${headerStyles.root} ${
+            absolute ? headerStyles.above : headerStyles.default
+          }`}
+        >
+          <div className={headerStyles.navContainer}>
+            <Link to="/" className={headerStyles.siteTitle}>
+              {siteTitle.toLowerCase()}
+            </Link>
+            <nav className={headerStyles.nav}>
+              <Link to="/">portfolio</Link>
+              <Link to="/about/">about me</Link>
+              <Link to="/contact/">contact</Link>
+              <Link to="/blog/">blog</Link>
+            </nav>
+          </div>
+          {false ? (
+            <button className="dark-switcher" onClick={theme.toggleDark}>
+              {theme.dark ? (
+                <span>Light mode ☀</span>
+              ) : (
+                <span>Dark mode ☾</span>
+              )}
+            </button>
+          ) : (
+            ""
+          )}
+        </header>
+      )}
+    </ThemeContext.Consumer>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string.isRequired,
+  absolute: PropTypes.bool,
+};
+
+Header.defaultProps = {
+  absolute: false,
 };
 
 export default Header;
